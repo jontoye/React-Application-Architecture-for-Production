9aad80addc2f92b92a77c092991461928ca2a1b2
Jon Toye <jontoye@gmail.com>
2024-07-21 14:50:04 -0400

    finish chapter 4 - building and configuring pages

--- code-stages/chapter-09-start/src/components/data-table/data-table.stories.tsx
+++ code-stages/chapter-09-start/src/components/data-table/data-table.stories.tsx
@@ -13,25 +13,26 @@ export default meta;
 
 const data = testData.jobs.slice(0, 6);
 
-const columns: DataTableProps<typeof data[0]>['columns'] =
-  [
-    {
-      title: 'Position',
-      field: 'position',
-    },
-    {
-      title: 'Department',
-      field: 'department',
-    },
-    {
-      title: 'Location',
-      field: 'location',
-    },
-  ];
+const columns: DataTableProps<
+  (typeof data)[0]
+>['columns'] = [
+  {
+    title: 'Position',
+    field: 'position',
+  },
+  {
+    title: 'Department',
+    field: 'department',
+  },
+  {
+    title: 'Location',
+    field: 'location',
+  },
+];
 
-const Template: Story<DataTableProps<typeof data[0]>> = (
-  props
-) => <DataTable {...props} />;
+const Template: Story<
+  DataTableProps<(typeof data)[0]>
+> = (props) => <DataTable {...props} />;
 
 export const Default = Template.bind({});
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { Center } from '@chakra-ui/react';
+
+import { Seo } from '@/components/seo';
+
+const LandingPage = () => {
+  return (
+    <>
+      <Seo title="Jobs App" />
+
+      <Center>
+        <p>rest of comp...</p>
+      </Center>
+    </>
+  );
+};
+
+export default LandingPage;
